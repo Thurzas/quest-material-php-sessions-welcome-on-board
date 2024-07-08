e076e3934a09a5bd60952ce62068d176e612b814
Thurzas <mathieu.miot@gmail.com>
2024-07-08 12:21:09 +0200

    Session form

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 //@todo start PHP session
 $errors = [];
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -17,6 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
     if (empty($errors)) {
         //@todo save contact form data to session
+        $_SESSION['contact'] = $contact;
         header('Location: result.php');
         exit();
     }
@@ -29,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome on board</title>
-    <link rel="stylesheet" href="/assets/styles/style.css">
+    <link rel="stylesheet" type="text/css" href="/assets/styles/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
